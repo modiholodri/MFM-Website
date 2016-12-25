7ef2deaa827a842428d2e2bab23ca02338bd3a8e
Modi777 <reinhold.lauer@gmail.com>
2016-12-25 17:07:21 +0700

    Cleaned up and deploy only visible html files

--- launch_counter.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$launch_count = @file_get_contents('launch_counter.txt');
-$launch_count++;
-@file_put_contents('launch_counter.txt', $launch_count);
-header('Location: http://movie-file-merger.org/MFMVersionRedirect.xml')
-
-?>
